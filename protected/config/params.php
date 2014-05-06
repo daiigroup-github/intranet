fd9f5c6694efd82662eb3c4a96f6537c9c18383a
kamyjapkung <kamyjap@gmail.com>
2014-05-06 09:12:13 +0700

    Theater Movie HR
    Theater Movie HR

--- protected/config/params.php
+++ protected/config/params.php
@@ -1,10 +1,16 @@
 <?php
+<<<<<<< HEAD
 return $params = array(
+=======
+return array(
+	'framework'=>'/../../Yii/yii-1.1.14.f0fee9/framework/yii.php',
+>>>>>>> FETCH_HEAD
 	'db'=>array(
 		'connectionString'=>'mysql:host=192.168.100.5;dbname=daiichi_dev',
 		'emulatePrepare'=>true,
 		'username'=>'root',
 		'password'=>'wfvb0b',
+<<<<<<< HEAD
 		'charset'=>'utf8',),
 //	'db'=>array(
 //		'connectionString'=>'mysql:host=localhost;dbname=daiichi_dev',
@@ -14,3 +20,7 @@ return $params = array(
 //		'charset'=>'utf8',),
 	'framework'=>"/../framework/framework/yii.php",
 );
+=======
+		'charset'=>'utf8',
+	));
+>>>>>>> FETCH_HEAD
