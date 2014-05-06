a94587cad327de0fb217274f7f163dfb8d21ccf6
kamyjapkung <kamyjap@gmail.com>
2014-05-06 09:18:24 +0700

    Resolved Confits

--- protected/config/params.php
+++ protected/config/params.php
@@ -1,16 +1,11 @@
 <?php
-<<<<<<< HEAD
-return $params = array(
-=======
 return array(
 	'framework'=>'/../../Yii/yii-1.1.14.f0fee9/framework/yii.php',
->>>>>>> FETCH_HEAD
 	'db'=>array(
 		'connectionString'=>'mysql:host=192.168.100.5;dbname=daiichi_dev',
 		'emulatePrepare'=>true,
 		'username'=>'root',
 		'password'=>'wfvb0b',
-<<<<<<< HEAD
 		'charset'=>'utf8',),
 //	'db'=>array(
 //		'connectionString'=>'mysql:host=localhost;dbname=daiichi_dev',
@@ -20,7 +15,5 @@ return array(
 //		'charset'=>'utf8',),
 	'framework'=>"/../framework/framework/yii.php",
 );
-=======
-		'charset'=>'utf8',
-	));
->>>>>>> FETCH_HEAD
+
+
