bf274cd76d4c8064d6164a576358fa7658aa866a
Kurtumm <kurtumm@gmail.com>
2014-07-24 15:47:37 +0700

    fit fast _chart

---
+++ protected/modules/fitfast/views/default/_chart.php
@@ -0,0 +1,65 @@
+<?php
+//options
+$size = 0;
+$lineWidth = 0;
+
+switch($span){
+    case 2:
+        $lineWidth = 28;
+        $size = 165;
+        break;
+    default:
+        $lineWidth = 48;
+        $size = 265;
+}
+?>
+
+<?php
+if($percent > 80)
+{
+	$lineColor = '#00aa00';
+}
+else if($percent > 50)
+{
+	$lineColor = '#ffee00';
+}
+else
+{
+	$lineColor = '#ff0000';
+}
+
+$id = isset($id) ? $id : '';
+
+$baseUrl = Yii::app()->baseUrl;
+$cs = Yii::app()->getClientScript();
+
+$cs->registerScriptFile($baseUrl . '/js/easypiechart/jquery.easypiechart.min.js');
+$cs->registerScript('pie'.$id, "
+$(function() {
+    //create instance
+    $('#chart{$id}').easyPieChart({
+        animate: 2000,
+		/*onStep: function(from, to, percent) {
+			this.el.children[0].innerHTML = Math.round(percent);
+		},*/
+		size:{$size},
+		barColor:'{$lineColor}',
+		lineCap : 'butt',
+		lineWidth: {$lineWidth},
+    });
+});
+");
+$cs->registerCSSFile($baseUrl."/css/chart{$span}.css");
+?>
+
+<div class="chart" id="chart<?php echo $id;?>" data-percent="<?php echo $percent; ?>">
+	<p class="percent"><?php echo $percent; ?></p>
+</div>
+
+<?php if(isset($grades)):?>
+<p style="text-align: center;">
+    <?php foreach ($grades as $k => $v): ?>
+        <span id="<?php echo $k;?>" class="label <?php echo ($k=='F') ? 'label-important' : 'label-success'?>"><?php echo $v . $k ?></span>
+    <?php endforeach; ?>
+</p>
+<?php endif;?>
